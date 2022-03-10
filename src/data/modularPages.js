2336f3d9bb6b08f8e1f9b35c8524eb05c5b206ab
timonforrer <timon.forrer@gmail.com>
2022-03-10 20:30:33 +0100

    Update query to fetch localised documents

--- src/data/modularPages.js
+++ src/data/modularPages.js
@@ -1,7 +1,7 @@
 const fetchFromSanity = require('../utils/fetchFromSanity');
 
 module.exports = function() {
-  const query = `*[_type == "modularPage"]`;
+  const query = `*[_type == "modularPage" && !(_id in path("drafts.i18n.*") && _id in path("i18n.**"))]`;
   const params = '';
   return fetchFromSanity('modular-page', query, params);
 }
