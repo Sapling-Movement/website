5f7673505df51d3a5ea25e81edfa0f1baacb23d8
timonforrer <timon.forrer@gmail.com>
2022-03-12 00:47:35 +0100

    Update query to resolve image url

--- src/data/blogPosts.js
+++ src/data/blogPosts.js
@@ -1,7 +1,18 @@
 const fetchFromSanity = require('../utils/fetchFromSanity');
 
 module.exports = function() {
-  const query = `*[_type == "blogPost" && !(_id in path("drafts.**"))]`;
+  const query = `*[_type == "blogPost" && !(_id in path("drafts.**"))]{
+    ...,
+    text[] {
+      ...,
+      _type == "imageWithCaption" => @{
+        image {
+          ...,
+          asset->
+        }
+      }
+    }
+  }`;
   const params = '';
   return fetchFromSanity('blog-post', query, params);
 }
