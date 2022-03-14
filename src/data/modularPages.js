2d2944b85feaea7a2ba281cb557dd35ee39cc82d
timonforrer <timon.forrer@gmail.com>
2022-03-14 13:02:43 +0100

    Add support for images to modularPages portable text

--- src/data/modularPages.js
+++ src/data/modularPages.js
@@ -1,7 +1,23 @@
 const fetchFromSanity = require('../utils/fetchFromSanity');
 
 module.exports = function() {
-  const query = `*[_type == "modularPage" && !(_id in path("drafts.**"))]`;
+  const query = `*[_type == "modularPage" && !(_id in path("drafts.**"))]{
+    ...,
+    body[]{
+      ...,
+      _type == "portableText" => @{
+        text[] {
+          ...,
+          _type == "imageWithCaption" => @{
+            image {
+              ...,
+              asset->
+            }
+          } 
+        }
+      }
+    }
+  }`;
   const params = '';
   return fetchFromSanity('modular-page', query, params);
 }
